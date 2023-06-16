7d850fa6d10fb662c674fa898d1b8e1517bd8e7b
Andrii Dziubenko <92578692+Kizliak@users.noreply.github.com>
2023-06-16 23:57:17 +0300

    Update harmony-bridge-bsc.py

--- harmony-bridge-bsc.py
+++ harmony-bridge-bsc.py
@@ -8,7 +8,7 @@ from sys import stderr
 
 time_delay_min = 120  # Min delay between each account
 time_delay_max = 180  # Max delay between each account
-repeats = 10 # How much runs this script will do. Must be > 0
+repeats = 1 # How much runs this script will do. Must be > 0
 amount = {"min": 0.0004, "max": 0.00043} # Amount to bridge min/max. Min value maust be 0.0004 and higher or tx fail!
 
 logger.remove()
